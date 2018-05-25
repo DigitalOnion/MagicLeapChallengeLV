c97984101ef58f4eb4fd32c09c849b9f43a0e76b
Luis Viruena <luis@motif.com>
2018-05-25 02:35:24 -0400

    Partial to pass Holder into DataBinding

---
+++ app/src/main/java/com/outerspace/magicleapchallengelv/viewmodel/HolderViewModel.java
@@ -0,0 +1,14 @@
+package com.outerspace.magicleapchallengelv.viewmodel;
+
+import com.outerspace.magicleapchallengelv.api.CoffeeMenu;
+
+import android.view.View;
+
+public class HolderViewModel implements HolderViewModelInterface {
+    private CoffeeMenu coffeeMenu;
+
+    @Override
+    public void onClickHolder(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/outerspace/magicleapchallengelv/viewmodel/HolderViewModelInterface.java
@@ -0,0 +1,9 @@
+package com.outerspace.magicleapchallengelv.viewmodel;
+
+import android.view.View;
+
+public interface HolderViewModelInterface {
+
+    public void onClickHolder(View view);
+
+}
